7c26733f477c045fec73333ddb10e75330840b79
Ibn-elaraby37 <h.ibrahim3705@gmail.com>
2025-08-12 02:41:46 +0300

    01 data types and vars

---
+++ 01 - data types and variables/data_types and variables.py
@@ -0,0 +1,36 @@
+# Data Types
+var01 = 'name'
+print(type(var01))
+print(var01)
+
+#print out strings values
+valx = 'name '
+valy = "name"
+valz = str("name")
+print(type(valx))
+print(type(valy))
+print(type(valz))
+
+# print numeric values
+x1 = 12
+x2 = 12.0
+x3 = 0.5
+x4 =int('12')
+x5 = float("13.02")
+x6 ="15.3"
+x7 = '13.0'
+# try to print each value and try to differentiate between each
+print(x1)
+print(x2)
+print(x3)
+print(x4)
+print(x5)
+print(x6)
+print(type(x1))
+
+print(type(x3))
+print(type(x5))
+print(type(x7))
+# are you able to do math on them >> just give it a try
+
+#that code was for getting knowledge to differentiate between different data types
