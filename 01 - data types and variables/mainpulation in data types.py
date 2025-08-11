4c0b3e2b01ba47d06d672da302da503273b7aeaf
Ibn-elaraby37 <h.ibrahim3705@gmail.com>
2025-08-12 02:53:07 +0300

    02 data types and vars

---
+++ 01 - data types and variables/mainpulation in data types.py
@@ -0,0 +1,11 @@
+# print your name and age in pyhon
+name ='Hammad '
+age = 25
+print(f"my name is:\n{name} \nAge is : {age}")
+
+# doing a simple math on values with datatypes
+height = 13.5
+width = 12.03
+print(f"Total of both width and height : \n {int (width ), float(height)}")
+# review the output and till the different
+
